2a3a79afeee4c1550fd496d33c01bca951b049f9
aaronchenwei <aaronchenwei@hotmail.com>
2018-05-15 07:20:25 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const fs = require('fs');
+
+const projectDir = path.resolve(fs.realpathSync(process.cwd()));
+
+const phaserModule = path.join(projectDir, '/node_modules/phaser-ce/');
+const phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+const pixi = path.join(phaserModule, 'build/custom/pixi.js');
+const p2 = path.join(phaserModule, 'build/custom/p2.js');
+
+/* phaser-ce Webpack configuration */
+const phaserCERewire = {
+  alias: {
+    pixi: pixi,
+    p2: p2,
+    'phaser-ce': phaser,
+  },
+  rules: [
+    {
+      test: /pixi\.js$/,
+      use: [
+        {
+          loader: 'expose-loader',
+          options: 'PIXI',
+        },
+      ],
+    },
+    {
+      test: /phaser-split\.js$/,
+      use: [
+        {
+          loader: 'imports-loader',
+          options: 'pixi,p2',
+        },
+        {
+          loader: 'expose-loader',
+          options: 'Phaser',
+        },
+      ],
+    },
+    {
+      test: /p2\.js$/,
+      use: [
+        {
+          loader: 'expose-loader',
+          options: 'p2',
+        },
+      ],
+    },
+  ],
+};
+
+function rewirePhaserCE(config, env) {
+  /* Spread syntax */
+  config.resolve.alias = { ...config.resolve.alias, ...phaserCERewire.alias };
+  config.module.rules = [...config.module.rules, ...phaserCERewire.rules];
+
+  return config;
+}
+
+module.exports = rewirePhaserCE;
